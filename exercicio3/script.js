1e05875e396d7ffadb2f2ef0bdcba782941c39ea
astron4ut <ctt.astro@gmail.com>
2022-12-20 22:54:42 -0300

    resolução do ex3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -7,26 +7,27 @@
 // };
 // ```
 
-// a) Crie uma cópia do objeto, acima, mantendo a estrutura, mas trocando **`nome`** por “Squirtle” e **`tipo`** por “Água”
-
-const pokemon1 = {
+const pokemon = {
     nome: "Bulbasaur",
     tipo: "Grama",
     nivel: 5
 };
 
-const pokemon1Copia = {
+// a) Crie uma cópia do objeto, acima, mantendo a estrutura, mas trocando **`nome`** por “Squirtle” e **`tipo`** por “Água”
+
+const pokemonCopia = {
+    ...pokemon,
     nome: "Squirtle",
-    tipo: "Água",
-    nivel: 5
-}
+    tipo: "Água"
+};
+
+console.log(pokemonCopia);
 
 // b) No objeto original, adicione mais uma propriedade, chamada `ataques`. Esta propriedade deve guardar um **array** que deve começar vazio.
 
-const ataques = [];
-pokemon1[ataques];
+pokemon.ataques = [];
 
-console.log(pokemon1);
+console.log(pokemon);
 
 // c-) crie um objeto de `ataque` com a estrutura abaixo e o adicione no array `ataques` utilizando `**push()**`
 
@@ -40,19 +41,27 @@ console.log(pokemon1);
 const ataque = {
     nome: "Investida",
     dano: 40,
-    tipo: "Água",
+    tipo: "Normal",
     precisao: 100
 };
 
-ataques.push(ataque);
-console.log(pokemon1);
-
 // c) Na cópia do objeto original, adicione a propriedade **`ataques`** a partir do **espalhamento** da propriedade de mesmo nome criada para o objeto original;
-
-
 
 // d) para o objeto original, adicione o ataque **“Folha Navalha”,** com **45 de dano**, **100 de precisão**, e de tipo **“Grama”**; no array de `ataques`
 
+pokemon.ataque = {
+    ataque: {
+        nome: "Folha Navalha",
+        dano: 45,
+        tipo: "Grama",
+        precisao: 100
+    }
+};
+
+console.log(pokemon);
+
 // e) para a cópia, crie o `ataque` **“Jato de Água”**, com **40 de dano**, **100 de precisão**, e do tipo **“Água”**; e adicione no array.
+
+
 
 // f) Imprima os dois objetos  do tipo pokemon no console.
